56dd790b6e9264bca71fec2d1c3bcd8e204498a4
Taylor Picard <taylorkpicard1998@gmail.com>
2023-07-14 17:14:38 -0500

    Mvp complete/ need separate comps

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,45 +4,96 @@ import axios from 'axios';
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
-  
-  constructor(){
-    super();
-    this.state = {
-      todos: []
+  state = {
+      todos: [],
+      error: '',
+      todoInput: '',
+      displayCompleted: true
     }
+  
+
+  changeToInput = evt => {
+    const {value} = evt.target
+    this.setState({...this.state, todoInput: value})
   }
 
-  fetchTodos = () => {
+  resetInput = () => {
+    this.setState({...this.state, todoInput: ''})
+  }
+
+  setResErr = (err) => {
+    this.setState({...this.state, error: err.response.data.message})
+  }
+
+  postTodo = () => {
+    axios.post(URL, {name: this.state.todoInput})
+      .then(res => {
+        this.setState({...this.state, todos: this.state.todos.concat(res.data.data)});
+        this.resetInput();
+      })
+      .catch(this.setResErr)
+  }
+  
+  todoSubmit = (evt) => {
+    evt.preventDefault();
+    this.postTodo();
+  }
+
+  getTodos = () => {
     axios.get(URL)
       .then(res => {
         this.setState({...this.state, todos: res.data.data});
       })
-      .catch(err => {
-        this.setState({...this.state, error: err.res.data.message})
+      .catch(this.setResErr)
+  }
+
+  toggleComplete = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if(td.id !== id) return td
+            return res.data.data
+          })
+        })
       })
+      .catch(this.setResErr)
+  }
+
+  toggleDisplayCompleted = () => {
+    this.setState({...this.state, displayCompleted: !this.state.displayCompleted})
   }
 
   componentDidMount() {
-    //fetch todos from server
-    this.fetchTodos();
+    this.getTodos();
   }
   
   render() {
     return (
       <div>
-        <div id='error'>Error: {this.state.error}</div>
+        <div id='error'>{this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {
-            this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
-            })
+            this.state.todos.reduce((acc,td) => {
+              if(this.state.displayCompleted || !td.completed) return acc.concat(
+                <div onClick={this.toggleComplete(td.id)} key={td.id}>{td.completed ? '✔️ ' : ''}{td.name}</div>
+              )
+              return acc
+            },[])
           }
         </div>
-        <form id='todoform'>
-          <input type='text' placeholder='New Todo'></input>
-          <input type='submit'></input>
-          <button>Clear Completed</button>
+        <form 
+          id='todoform'
+          onSubmit={this.todoSubmit}>
+          <input 
+            type='text' 
+            placeholder='New Todo' 
+            value={this.state.todoInput}
+            onChange={this.changeToInput}
+          />
+          <input type='submit'/>
+          <button onClick={this.toggleDisplayCompleted}>{this.state.displayCompleted ? 'Hide':'Show'} Completed</button>
         </form>
       </div>
     )
